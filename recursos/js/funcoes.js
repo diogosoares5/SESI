d1edb31b51dc588cec96b53d5d641c87e99202dd
diogo <diogo.oliveira.soares@outlook.com>
2013-03-21 17:24:06 -0300

    ajuste finalizações

--- recursos/js/funcoes.js
+++ recursos/js/funcoes.js
@@ -1,53 +1,42 @@
-		jQuery.noConflict();
-		jQuery(document).ready(function(){
-		jQuery(".item").hide();
-		jQuery("li").removeClass("ativo");
-		jQuery("#B1").addClass("ativo");
-		jQuery("#aba-b1").fadeIn();
-		jQuery("#link-b1").click(function(){
+jQuery.noConflict();
+
+jQuery(document).ready(function(){
+	
+	jQuery(".item").hide();
+	jQuery("li").removeClass("ativo");
+	jQuery("#link-home").addClass("ativo");
+	jQuery("#aba-home").fadeIn();
+	
+	jQuery("#link-home").click(function(){
 		jQuery("li").removeClass("ativo");
 		jQuery(this).addClass("ativo");
 		jQuery(".item").hide();
-		jQuery("#aba-b1").fadeIn();
-		});
-		jQuery("#link-b2").click(function(){
-		jQuery("li").removeClass("ativo");
-		jQuery(this).addClass("ativo");
-		jQuery(".item").hide();
-		jQuery("#aba-b2").fadeIn();
-		});
-		jQuery("#link-b3").click(function(){
-		jQuery("li").removeClass("ativo");
-		jQuery(this).addClass("ativo");
-		jQuery(".item").hide();
-		jQuery("#aba-b3").fadeIn();
-		});
-		jQuery("#link-b4").click(function(){
-		jQuery("li").removeClass("ativo");
-		jQuery(this).addClass("ativo");
-		jQuery(".item").hide();
-		jQuery("#aba-b4").fadeIn();
-		});		
-		});
-
-
-		jQuery(document).ready(function(){
-		  jQuery("#hide").click(function(){
-		    jQuery(".list2").toggle(500);
-		  });
-		});
-
-
-	jQuery(document).ready(function() {
-	    jQuery('#mycarousel').jcarousel({
-			wrap: 'circular'
-	    });
+		jQuery("#aba-home").fadeIn();
 	});
+	jQuery("#link-programa").click(function(){
+		jQuery("li").removeClass("ativo");
+		jQuery(this).addClass("ativo");
+		jQuery(".item").hide();
+		jQuery("#aba-programa").fadeIn();
+	});
+	jQuery("#link-historia").click(function(){
+		jQuery("li").removeClass("ativo");
+		jQuery(this).addClass("ativo");
+		jQuery(".item").hide();
+		jQuery("#aba-historia").fadeIn();
+	});
+	jQuery("#link-comunidades").click(function(){
+		jQuery("li").removeClass("ativo");
+		jQuery(this).addClass("ativo");
+		jQuery(".item").hide();
+		jQuery("#aba-comunidades").fadeIn();
+	});
+	jQuery("#link-contato").click(function(){
+		jQuery("li").removeClass("ativo");
+		jQuery(this).addClass("ativo");
+		jQuery(".item").hide();
+		jQuery("#aba-contato").fadeIn();
+	});
+	
 
-	jQuery(document).ready(function(){	
-	jQuery('#click').click(function() {
-	  jQuery('#TPart').slideDown('slow', function() {
-	    jQuery('#click').hide()
-	  });
-	})
-	})
+});
