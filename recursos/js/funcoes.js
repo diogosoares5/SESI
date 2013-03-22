288ae95a2ad2ef53b6924fe98ba9d4cab6580c2a
diogo <diogo.oliveira.soares@outlook.com>
2013-03-22 11:27:04 -0300

    termino dos ajustes para scroll

--- recursos/js/funcoes.js
+++ recursos/js/funcoes.js
@@ -18,12 +18,14 @@ jQuery(document).ready(function(){
 		jQuery(this).addClass("ativo");
 		jQuery(".item").hide();
 		jQuery("#aba-programa").fadeIn();
+		jQuery('#scrollbar1').tinyscrollbar();
 	});
 	jQuery("#link-historia").click(function(){
 		jQuery("li").removeClass("ativo");
 		jQuery(this).addClass("ativo");
 		jQuery(".item").hide();
 		jQuery("#aba-historia").fadeIn();
+		jQuery('#scrollbar2').tinyscrollbar();
 	});
 	jQuery("#link-comunidades").click(function(){
 		jQuery("li").removeClass("ativo");
@@ -37,6 +39,5 @@ jQuery(document).ready(function(){
 		jQuery(".item").hide();
 		jQuery("#aba-contato").fadeIn();
 	});
-	
-	
+		
 });
